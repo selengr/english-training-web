90e3617b03d465c40ddd6a517dda8c98341bf412
reza <78854869+selengr@users.noreply.github.com>
2023-06-23 09:06:34 +0330

    update 10

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import {useEffect} from "react"
 import Image from 'next/image'
 import axios from "axios"
 import useSWR from "swr";
-import { Email } from "@/nodemailer/Email";
+import { Email } from "@/styles/components/nodemailer/Email";
 
 
 
@@ -24,32 +24,32 @@ export default function Home() {
     } catch (error) {
       console.error(error);
     }
-  };
+//   };
 
-  useEffect(()=>{
+//   useEffect(()=>{
  
-  },[])
+//   },[])
 
 
-  const handlePost2 = async () => {
-    try {
-      const response = await axios.get('/api/auth/register');
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//   const handlePost2 = async () => {
+//     try {
+//       const response = await axios.get('/api/auth/register');
+//       console.log(response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  // if (error) return <div>Error loading data</div>;
-  // if (!data) return <div>Loading...</div>;
+//   // if (error) return <div>Error loading data</div>;
+//   // if (!data) return <div>Loading...</div>;
 
 
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-aaaaaaaaaaaaaaa
-            {/* <Email /> */}
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+// aaaaaaaaaaaaaaa
+//             {/* <Email /> */}
  
-    </main>
+//     </main>
   )
 }
