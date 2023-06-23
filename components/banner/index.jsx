de8d0c9078f24b43f35a2c7afef32f178cde72c6
reza <78854869+selengr@users.noreply.github.com>
2023-06-23 09:16:44 +0330

    update 12

--- components/banner/index.jsx
+++ components/banner/index.jsx
@@ -1,28 +1,50 @@
+// 'use client'
 import Image from "next/image";
 import styles from "../../styles/components/banner/banner.module.css"
+import { HeroHeader } from "../custom/hero-header";
 
 
-const Banner = () => {
+const Banner = ({data}) => {
+  console.log("rr",data)
     return (
         <div className={styles["landing-main"]}>
-             <Image
-                // loader={myLoader}
-                src="/images/https___s3-us-west-2.avif"
-                alt="Picture of the author"
-                width={500}
-                height={500}
-                className={styles["landing-img"]}
-                />
+          {data === "its me" && <div className={`${styles["landing-img"]} `}> <HeroHeader /></div>}
+          {data !== "its me" && 
+              <Image
+              // loader={myLoader}
+              src={"/images/https___s3-us-west-2.avif"}
+              alt="Picture of the author"
+              width={500}
+              height={500}
+              className={styles["landing-img"]}
+              />
+          }
 
-                <>
-                <Image
-                  src={"/images/fikeus-west-2.avif"}
-                  alt="Picture of the author"
-                  width={0}
-                  height={0}
-                  className={styles["landing-div-rounded"]}
-                />
-                </>
+          {data === "its me" && 
+              <Image
+              // loader={myLoader}
+              src={"/images/E52075F8-14EA-496F-A10C-CB4405AFE196_1_105_c.jpeg"}
+              alt="Picture of the author"
+              width={500}
+              height={500}
+              className={styles["landing-div-rounded"]}
+              />
+          }
+
+          {data !== "its me" && 
+              <Image
+              src={"/images/fikeus-west-2.avif"}
+              alt="Picture of the author"
+              width={0}
+              height={0}
+              className={styles["landing-div-rounded"]}
+            />
+          }
+             
+
+               
+
+                
 
                 <h1 className={styles["landing-title"]}>commercial web</h1>
 
--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,10 +1,34 @@
+import Image from "next/image";
 import styles from "../../styles/components/header/header.module.css"
+import Link from "next/link";
 
 const Header = () => {
     return (
         <>
             <div className={styles["landing-top"]}>
-               ddd
+               {/* ddd   */}
+
+               <div className=" fixed sm:right-28 right-10 flex justify-center align-middle items-center overflow-hidden">
+                
+               <Link href="/about"
+               className="overflow-hidden"
+               > <label >About</label></Link>
+                
+
+                 <label className="sm:ml-2 ml-0">Contact</label>
+               </div>
+
+
+               <div className="cover-individuals fixed sm:left-12 left-4 flex justify-center align-middle items-center">
+                <Image
+                    src={"/images/loading/Loading-own.gif"}
+                    alt="author"
+                    width={50}
+                    height={50}
+                    className="rounded-full"
+                />
+                 <label className="sm:ml-2">reza karbakhsh</label>
+               </div>
             </div>
 
            
