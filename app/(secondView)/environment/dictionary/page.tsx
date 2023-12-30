b471ea94ba2a563e0737a0862fa7f4f437748247
selengr <reza1997karbakhsh@gmail.com>
2023-12-30 11:13:41 +0330

    fix

--- app/(secondView)/environment/dictionary/page.tsx
+++ app/(secondView)/environment/dictionary/page.tsx
@@ -1,123 +1,148 @@
-"use client"
+"use client";
 
 import UICustomizedCombo from "@/components/custom/customized_combo";
-import { Box, Button, Card, CardContent, CardHeader, Input, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Input,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Audio from 'react-audio-player';
+import Audio from "react-audio-player";
+
+
+type Definition = {
+  definition: string;
+  synonyms: string[];
+  antonyms: string[];
+  example?: string;
+};
+
+type Meaning = {
+  partOfSpeech: string;
+  definitions: Definition[];
+  synonyms: string[];
+  antonyms: string[];
+};
+
+type License = {
+  name: string;
+  url: string;
+}|any;
+
+type Phonetic = {
+  text: string;
+  audio: string;
+  sourceUrl: string;
+  license: License;
+};
+
+type Datum = {
+  word: string;
+  phonetic: string;
+  phonetics: Phonetic[];
+  meanings: Meaning[];
+  license: License;
+  sourceUrls: string[];
+};
+
+
 
 export default function Page() {
-    const [word, setWord] = useState('');
-    const [definition, setDefinition] = useState({
-      word: '',
-      phonetic: '',
-      meanings: [],
-      audioUrls: [],
-    });
-
-    // const staticData = await fetch(`https://...`, { cache: 'force-cache' })
-    // const dynamicData = await fetch(`https://...`, { cache: 'no-store' })
-    // const revalidatedData = await fetch(`https://...`, {
-    //   next: { revalidate: 10 },
-    // })
+  const [definition, setDefinition] = useState<Datum>();
 
 
+  useEffect(() => {
+    async function facilitise() {}
+    facilitise();
+  }, []);
 
-useEffect(()=>{
-    async function facilitise(){
-        
-    }
-    facilitise()
-},[])
-
-
-const handleSearch = (word:string) => {
-    const splitWord = word.toLowerCase().split(' ');
-    
-    // const url = `/dictionary/${splitWord.join('+')}`;
-    // router.push(url);
+  const handleSearch = async (word: string) => {
 
     try {
-        // const dynamicData = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/word`, { cache: 'no-store' })
-        // console.log("data",dynamicData)
-           const response =  axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/word`);
-           console.log(response)
+      // const dynamicData = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/word`, { cache: 'no-store' })
+      // console.log("data",dynamicData)
+      const response = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      );
+      setDefinition({
+        license : response.data[0].license,
+        meanings: response.data[0].meanings,
+        phonetics: response.data[0].phonetics,
+        phonetic : response.data[0].phonetic,
+        sourceUrls : response.data[0].sourceUrls,
+        word: response.data[0].word,
+
+
+      });
+      renderDefinitions()
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   };
 
 
-const handleDefinitionChange = (data) => {
-    setDefinition(data);
-  };
+
 
   const renderDefinitions = () => {
-    if (!definition.word) {
+    console.log('definition :>> ', definition);
+    // console.log('definition.meanings :>> ', definition.meanings);
+    
+    if (!definition?.word) {
       return (
         <div>
           <Typography variant="h4">Loading...</Typography>
         </div>
       );
     }
-
     return definition.meanings.map((meaning, index) => (
       <div key={index} className="mb-2">
-        <Typography variant="subtitle1">
-          {meaning.partOfSpeech}
-        </Typography>
+        <Typography variant="subtitle1">{meaning?.partOfSpeech}</Typography>
         <ul>
-          {meaning.definitions.map((definition, i) => (
+          {meaning?.definitions.map((definition, i) => (
             <li key={i}>
-              {definition}
-              {i < meaning.definitions.length - 1 ? ', ' : ''}
+              {definition.definition}
+              {/* {i < meaning.definitions.length - 1 ? ", " : ""} */}
             </li>
           ))}
         </ul>
       </div>
     ));
+
   };
 
   const renderAudioPlayer = () => {
-    if (!definition.audioUrls) {
+    if (!definition?.phonetics) {
       return null;
     }
 
-    return definition.audioUrls.map((audioUrl, index) => (
-      <Audio
-        key={index}
-        src={audioUrl}
-        controls
-        className="mb-2"
-      />
+    return definition.phonetics.map((audioUrl, index) => (
+      <Audio key={index} src={audioUrl.audio} controls className="mb-2" />
     ));
   };
 
-    
-   
-    return (
-        <Box>
-          <h1>test</h1>
-          <Input
-            placeholder="Enter the word you want to search for"
-            onChange={(event) => setWord(event.target.value)}
-          />
-          {/* <Button type="submit" onClick={handleSearch}>Search</Button> */}
-          {word &&
-            <Card>
-              <CardHeader>{definition.word}</CardHeader>
-              <CardContent>ss
-                {renderDefinitions()}
-                {renderAudioPlayer()}
-              </CardContent>
-            </Card>}
+  return (
+    <Box>
+      <h1>test</h1>
+      <Input
+        placeholder="Enter the word you want to search for"
+        onChange={(event) => handleSearch(event.target.value)}
+      />
+      {definition?.word && (
+        <Card>
+          <CardHeader>{definition.word}</CardHeader>
+          <CardContent>
+          
+            {renderDefinitions()}
+            {renderAudioPlayer()}
+          </CardContent>
+        </Card>
+      )}
 
-            <UICustomizedCombo
-
-            placeholder={'جستجوی اعتبار'}
-            label="لطفا اعتبارات خود را انتخاب نمائید"
-
-          />
-        </Box>
-      );
-  }
+    </Box>
+  );
+}
