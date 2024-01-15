e87a1c8bf3c3e6442eed217da2f38037370c05a4
selengr <reza1997karbakhsh@gmail.com>
2024-01-15 10:26:32 +0330

    fix

--- services/utils/VSCodeFileUpload.ts
+++ services/utils/VSCodeFileUpload.ts
@@ -1,15 +1,12 @@
 
 
 
-var express = require('express');
-var app = express();
-var multer = require('multer')
-var cors = require('cors');
-1
-2
-3
-4
-var express = require('express');
-var app = express();
-var multer = require('multer')
-var cors = require('cors');
+// var express = require('express');
+// var app = express();
+// var multer = require('multer')
+// var cors = require('cors');
+
+// var express = require('express');
+// var app = express();
+// var multer = require('multer')
+// var cors = require('cors');
