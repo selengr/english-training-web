b296bfde487792db553accd3dbf72720879db719
selengr <reza1997karbakhsh@gmail.com>
2024-01-15 17:04:33 +0330

    sdff

--- next.config.js
+++ next.config.js
@@ -17,18 +17,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-
-
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     experimental: {
-//       appDir: true,
-//     //   topLevelAwait: true,
-//     }
-     
-//   }
-  
-//   module.exports = nextConfig
