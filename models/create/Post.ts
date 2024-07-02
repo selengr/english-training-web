ddb694590d747fda6d1cdc7f5e032f0e0f3f6a3f
reza <reza1997karbakhsh@gmail.com>
2024-07-02 16:47:50 +0330

    fix

--- models/create/Post.ts
+++ models/create/Post.ts
@@ -15,7 +15,6 @@ const TableDataSchema = new mongoose.Schema({
   column3: { type: String, required: false }
 });
 
-
 const PostModel = new mongoose.Schema({
   cover: { type: String, required: true },
   banner:  { type: String, required: true },
@@ -32,32 +31,9 @@ const PostModel = new mongoose.Schema({
     email: { type: String, required: false }
   },
   languageLevel: { type: String, required: false },
-  // saveExample: { type: Array<any>, required: false },
-  // tableData: { type: Array<any>, required: false },
-  // saveExample: ExamSchema,
-  // tableData: TableDataSchema,
   descriptionLink: String,
   link: String,
   tags: { type: Array<String>, required: false }
-
-  // cultureNotes: { type: String, required: false },
-  // outline: { type: String, required: false },
-  // callToAction: { type: String, required: false },
-  // slug: { type: String, required: false, unique: false },
-  // likes: { type: String, required: false },
-  // views: { type: String, required: false },
-  // status: { type: String, required: false },
-  // excerpt: { type: String, required: false },
-  // featuredImage: { type: String, required: false },
-  // categories: [{ type: String, required: false }],
-  // lastUpdateDate: { type: Date, required: false },
-  // creation: { type: Date, required: false },
-  // comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-  // metadata: { type: mongoose.Schema.Types.Mixed, required: false },
-  // learningObjective: { type: String, required: false },
-  // vocabularyFocus: { type: String, required: false },
-  // cover: { type: mongoose.Types.ObjectId, ref: 'File', required: true},
-  // banner: { type: mongoose.Types.ObjectId, ref: 'File', required: true}
 }, { timestamps: true });
 
 export default mongoose.models.Post || mongoose.model("Post", PostModel);
