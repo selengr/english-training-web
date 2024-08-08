eac13debe06fb2330cec91c5cadb75cec6367b20
reza <reza1997karbakhsh@gmail.com>
2024-08-09 01:57:28 +0330

    dd

--- actions/post-action.ts
+++ actions/post-action.ts
@@ -1,43 +1,43 @@
-'use server';
+// 'use server';
 
-import prisma from '@/lib/prisma';
-import { revalidatePath } from 'next/cache';
+// import prisma from '@/lib/prisma';
+// import { revalidatePath } from 'next/cache';
 
-export const CreatePostAction = async (formdata: FormData) => {
-  try {
-    const { title, body, iamge } = Object.fromEntries(formdata);
+// export const CreatePostAction = async (formdata: FormData) => {
+//   try {
+//     const { title, body, iamge } = Object.fromEntries(formdata);
 
-    const address = String(title).split(' ').join('-');
+//     const address = String(title).split(' ').join('-');
 
-    const post = await prisma.post.create({
-      data: {
-        title: title as string,
-        address,
-        body: body as string,
-        image: iamge as string,
-      },
-    });
+//     const post = await prisma.post.create({
+//       data: {
+//         title: title as string,
+//         address,
+//         body: body as string,
+//         image: iamge as string,
+//       },
+//     });
 
-    if (!post) return { success: false };
+//     if (!post) return { success: false };
 
-    revalidatePath('/admin');
+//     revalidatePath('/admin');
 
-    return { success: true };
-  } catch (error) {
-    console.log('CreatePostAction', error);
-  }
-};
+//     return { success: true };
+//   } catch (error) {
+//     console.log('CreatePostAction', error);
+//   }
+// };
 
-export const DeletePostAction = async (id: number) => {
-  try {
-    await prisma.post.delete({
-      where: {
-        id,
-      },
-    });
+// export const DeletePostAction = async (id: number) => {
+//   try {
+//     await prisma.post.delete({
+//       where: {
+//         id,
+//       },
+//     });
 
-    revalidatePath('/admin');
-  } catch (error) {
-    console.log('DeletePostAction', error);
-  }
-};
+//     revalidatePath('/admin');
+//   } catch (error) {
+//     console.log('DeletePostAction', error);
+//   }
+// };
