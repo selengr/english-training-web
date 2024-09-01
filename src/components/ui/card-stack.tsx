a7aeca3cdb0b9f93e2ba8ed2dd788ef8f47d91ad
reza <reza1997karbakhsh@gmail.com>
2024-09-01 20:04:21 +0330

    bg-[#f8f8f8]

--- src/components/ui/card-stack.tsx
+++ src/components/ui/card-stack.tsx
@@ -45,7 +45,7 @@ export const CardStack = ({
         return (
           <motion.div
             key={card.id}
-            className="absolute dark:bg-black bg-white h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
+            className="absolute dark:bg-[#2f3437] bg-[#f8f8f8] h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
             style={{
               transformOrigin: "top center",
             }}
