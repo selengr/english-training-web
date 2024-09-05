5e1d77af8719245ad5ffc62c421ee907a1f03352
reza <reza1997karbakhsh@gmail.com>
2024-09-05 17:55:28 +0330

    npm install -g @liara/cli

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+   output : "standalone",
     images: { remotePatterns: [ {
         protocol: 'https',
         hostname: 'wtgtkec1ncke0twm.public.blob.vercel-storage.com'
